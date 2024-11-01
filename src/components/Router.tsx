bed9b0381b2313c299044db35a9e16e7ac0096c4
Kamomiruku <kamomirukusan@gmail.com>
2024-11-01 13:56:04 +0300

    first commit

---
+++ src/components/Router.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./css/Router.css"
+
+export default function Router() {
+
+  return (
+    <>
+      <div className="router">
+         <hr />
+         <div className="home route">
+            <Link to="/"></Link>
+            <img src="../public\home.svg" className="homeicon icon" />
+         </div>
+         <div className="profile route">
+            <Link to="/me"></Link>
+            <img src="../public\circle-user-round.svg" className="profileicon icon" />
+         </div>
+      </div>
+    </>
+  )
+}
