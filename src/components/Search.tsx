1808bb903855ed383d1dbcd604bb13f63dd4a1d9
Kamomiruku <kamomirukusan@gmail.com>
2024-11-07 17:28:04 +0300

    search added

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,22 +1,47 @@
+import { useState } from "react"
+import "./css/Search.css"
 
 export default function Search() {
+
+   const [FormFocus, setFormFocus] = useState(0)
+
   return (
     <div>
-      <input type="search" name="search" className="search" placeholder="Введите текст..." />
-      <div className="advancedSearch">
-         <label>
-            <input type="text" name="author" id="author" />
-            От автора
-         </label>
-         <div className="type">
-            <input type="radio" name="" id="1" />
-            <label htmlFor="1"></label>
-            <input type="radio" name="" id="2" />
-            <label htmlFor="2"></label>
-            <input type="radio" name="" id="3" />
-            <label htmlFor="3"></label>
+      <button className="hideSearchForm" onClick={() => setFormFocus(0)}></button>
+      <form className="searchForm" onClick={() => setFormFocus(1)}>
+         <style>
+            {`
+               .searchForm::before {
+                  opacity: ${FormFocus}
+               }
+
+               .advancedSearch {
+                  transform: scaleY(${FormFocus})
+               }
+            `}
+         </style>
+         <form className="mainSearch">
+            <input type="text" name="search" className="search" placeholder="Введите текст..." autoComplete="false" />
+            <input type="reset" className="clear" />
+         </form>
+         <span className="searchIcon">
+            <input type="submit" value="" id="searchSubmit" />
+         </span>
+         <div className="advancedSearch">
+            <div className="from-author">
+               <label htmlFor="author" id="authorLabel">От автора</label>
+               <input type="text" name="author" id="author" autoComplete="false" />
+            </div>
+            <div className="types">
+               <input type="radio" name="type" id="1" className="radio" checked />
+               <label htmlFor="1" className="type">Трэк</label>
+               <input type="radio" name="type" id="2" className="radio" />
+               <label htmlFor="2" className="type">Альбом</label>
+               <input type="radio" name="type" id="3" className="radio" />
+               <label htmlFor="3" className="type">Исполнитель</label>
+            </div>
          </div>
-      </div>
+      </form>
       <div className="queries">
          {
             localStorage.getItem("queries")
